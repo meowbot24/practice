0d7d745b2deca5a3fc18113877491fc39c4f293b
Cecilia Wong <ceciliakhwong@gmail.com>
2016-07-22 12:05:21 -0400

    Fixing filename change for ItemList.js

--- cash-register/js/ItemList.js
+++ cash-register/js/ItemList.js
@@ -1,9 +1,9 @@
 // object containing all items available for sale
 var itemList = {
   milk: new Item('milk', 2.00),
-    eggs: new Item('eggs', 3.00),
-    chocolate: new Item('chocolate', 2.50),
-    kittyFood: new Item('kitty food', 20.00)
+  eggs: new Item('eggs', 3.00),
+  chocolate: new Item('chocolate', 2.50),
+  kittyFood: new Item('kitty food', 20.00)
 };
 
 // add new items to itemList
@@ -14,9 +14,3 @@ var addNewItem = function(name, price) {
 
 addNewItem('candy',4);
 addNewItem('popcorn',5);
-
-// // to view/log itemList
-// for (var x in itemList) {
-//  console.log(itemList[x]['name']);
-//  console.log(itemList[x]['price']);
-// };
